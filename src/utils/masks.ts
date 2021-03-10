f0cdca38078fc1a20658be1beaeae3e97dd786b8
João Vitor Sousa <joaovfsousa@gmail.com>
2021-03-10 19:22:08 -0300

    Add date masks

--- src/utils/masks.ts
+++ src/utils/masks.ts
@@ -6,3 +6,18 @@ export const cpfMask = (value: string) => {
     .replace(/(\d{3})(\d{1,2})/, '$1-$2')
     .replace(/(-\d{2})\d+?$/, '$1');
 }
+
+export const brDateMask = (value: string) => {
+  return value
+    .replace(/\D/g, '')
+    .replace(/(\d{2})(\d)/, '$1/$2') 
+    .replace(/(\d{2})(\d)/, '$1/$2')
+}
+
+export const usToBrDate = (value: string) => {
+  const year = value.substring(0, 4)
+  const month = value.substring(5, 7)
+  const day = value.substring(8, 10);
+
+  return [day, month, year].join('/');
+}
