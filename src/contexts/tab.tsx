ca4f14f614aa37e730dab95689ee955d7f94cda4
João Vitor Sousa <joaovfsousa@gmail.com>
2021-04-14 16:39:16 -0300

    Add get tabs from cloud functionality

--- src/contexts/tab.tsx
+++ src/contexts/tab.tsx
@@ -1,13 +1,27 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
+import { ITabData } from '../interfaces/tab';
+import * as tabService from '../services/tab';
 
 interface ITabContextData {
+  tabs: ITabData[];
 }
 
 const TabContext = createContext<ITabContextData>({} as ITabContextData);
 
 export const TabProvider: React.FC = ({ children }) => {
+  const [ tabs, setTabs ] = useState<ITabData[]>([]);
+
+  async function getTabs(){
+    const response = await tabService.get_all();
+    setTabs(response);
+  }
+
+  useEffect(() => {
+    getTabs();
+  }, []);
+
   return (
-    <TabContext.Provider value={{}}>
+    <TabContext.Provider value={{tabs}}>
       {children}
     </TabContext.Provider>
   );
